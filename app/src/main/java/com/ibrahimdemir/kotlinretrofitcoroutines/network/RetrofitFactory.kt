71bd41085f409a273db344b4b8a5bcaba47362ec
ibrahim demir <ibrahim.demir@vakifbank.com.tr>
2020-12-29 09:53:57 +0300

    Retroft httpLoggingInterceptor was added

--- app/src/main/java/com/ibrahimdemir/kotlinretrofitcoroutines/network/RetrofitFactory.kt
+++ app/src/main/java/com/ibrahimdemir/kotlinretrofitcoroutines/network/RetrofitFactory.kt
@@ -1,6 +1,8 @@
 package com.ibrahimdemir.kotlinretrofitcoroutines.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,9 +10,21 @@ object RetrofitFactory {
 
     private const val BASE_URL = "https://yts.am/api/v2/"
 
+    private val interceptor = run {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addNetworkInterceptor(interceptor)
+        .build()
+
     fun makeRetrofitService(): RetrofitService {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
